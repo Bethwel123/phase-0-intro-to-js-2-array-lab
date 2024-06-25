0794ae8ff9fc987afcc589be090fd170d0989014
Bethwel123 <bethwellangat654@gmail.com>
2024-06-26 02:09:17 +0300

    Practice arrays with javaScript

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+ }
+
+ function destructivelyRemoveLastCat(){
+  cats.pop();
+  return cats;
+}
+ 
+ function destructivelyPrependCat(name){
+   cats.unshift(name);
+   return cats;
+ }
+
+ function destructivelyRemoveFirstCat(){
+  cats.shift();
+  return cats;
+}
+
+function appendCat(name){
+  var name_1= [...cats,name];
+  return name_1;
+}
+
+function prependCat(name){
+  var name_2=[name, ...cats];
+  return name_2
+}
+
+function removeLastCat() {
+  const newArray = [...cats];
+  
+  newArray.pop();
+  
+  return newArray;
+}
+
+function removeFirstCat(){
+  const newCats = [...cats];
+
+  newCats.shift();
+  return newCats;
+}
+
+  
+ 
